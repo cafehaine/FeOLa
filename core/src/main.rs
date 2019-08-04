a00c5549b3210e631ef2fe1d11510465b38e5b9e
cafehaine <kilian.guillaume@gmail.com>
2019-08-04 15:45:51 +0200

    Use source instead of module, add docstrings

--- core/src/main.rs
+++ core/src/main.rs
@@ -3,18 +3,23 @@ use std::env;
 use std::path::Path;
 use std::result::Result;
 
+/// Load a single source, given it's path.
 fn load_module(p: &Path) {
     println!("{}", p.display());
 }
 
-fn load_modules() {
+/// Load all the sources available in the paths.
+///
+/// The paths are at the moment specified using the `FEOLA_SOURCE_PATH`
+/// environment variable.
+fn load_sources() {
     match env::var_os("FEOLA_SOURCE_PATH") {
         Some(paths) => {
             for path in env::split_paths(&paths) {
                 let pattern = format!("{}/*.so", path.display());
 
                 for entry in glob(&pattern).unwrap().filter_map(Result::ok) {
-                    load_module(&entry);
+                    load_source(&entry);
                 }
             }
         }
@@ -23,11 +28,11 @@ fn load_modules() {
 }
 
 fn main() {
-    // Load modules
-    load_modules();
+    // Load sources
+    load_sources();
     // Setup pipes for the frontends
     // Poll the pipes (or maybe there's a way to wait on events?)
-    // Transmit the query from the pipe to all of the modules
+    // Transmit the query from the pipe to all of the sources
     // Aggregate the results
     // Serialize the results to JSON
     // Send the results to the frontend that requested it
